cda375b04032a6063fa37578e13561af64534737
Nnalue Kingsley <164803861+Ebukannalue1132@users.noreply.github.com>
2024-07-25 22:58:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/logo.jpg";
 global.devs = "923072380380";
 global.sudo = process.env.SUDO || "923072380380";
-global.owner = process.env.OWNER_NUMBER || "923072380380";
+global.owner = process.env.OWNER_NUMBER || "2349070810971";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -57,7 +57,7 @@ global.SESSION_ID =
 
 
   
-  "Your session ID" //Make sure session id starts with Byte;;;
+  "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY0N6UFJpSlZaWlVIZ3BLQXZqbFFDZnc0ZXMxZTZkU05PVHRHMEFQa2RtMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUVJBYUp5TUNmQmsvSU80dWNmeHE4ZXJMNk8zNjFSU3RFRi9OallxL3JHbz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJZQmNHNXV3bkpSMEJuUU1MVnh2cVViN0R4SEMvYWZBVFQ0dnRmM2tFK2tZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsREwyMHNEWkFZc3YrS0NsSHJWd0M2ZU5YY3UrbzczbHVaN0lKb0syM0FJPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhEazd0dFE5aTJIdHd5Um13SHhiaEJER3l6Z0ptdEhVQlBCdEZQTTAyWEE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImMxSFdaTDM0Sy9ZekpVdytxd1VDRGNIYzhvdktGenhUT1FFOW5oMHhaVVk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia09VWW42Z2pGWnN0UWFUR01BbnVvYmkwUlhtQnNSU3NxQWU0NGVVUlNuaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoickp5OWRxN1Mydm5RYUlDVmx2R1N4U3dPZ010cjdaY2F5UDA3UEJFa0pUZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InR2NHowUHQ3bmZCcXRiSFF6TjhnZ3pJdG5icGY5TlFJYlVLQkt2L20vTE1wWlNLZk16cjAxMjNGMHlHaHA4clNzZmIxY3FuUld4K0lNaU80SXV4cEFRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTQ1LCJhZHZTZWNyZXRLZXkiOiJDSjdISUpuM2NaM0c3YXJ5M0ZseEdrSjJ6RFdSS253UHNQV3draXdkN0tFPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJEQVRaRGxwdVJ3Mm5ZSGpnakhaTUhBIiwicGhvbmVJZCI6IjYyOWIyNjMyLWRhY2MtNGJjZC1iYjUwLTUwOTM1ZDBiZmQ2ZSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI5cWFZRFJJYWJ5UVJtei8zOThOY2lkbU95dHc9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicU5adTR2UkFNWjEvQ1g4THgycTY5NDQ0RDJJPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkpRWVdBVzhRIiwibWUiOnsiaWQiOiIyMzQ5MDcwODEwOTcxOjc5QHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNOMjF2YjRDRUtLU2k3VUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJPb1E3OUJUd3NpZW9PbUNKcS9JZlhYaFRFUEJTZU1vQ1VzL2lWT0hSbTNBPSIsImFjY291bnRTaWduYXR1cmUiOiI5eWgrQ3NrRGZTNVVaVkJ6NTF6ajhoQW43T0hRdEJYUndRTkRHbG9ERmR2dm5YZzBOL0VmTW1kS2VXUjlPN3RvNXplRnIvczlHQ3FTQVBOd0s4UFVBQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiMFo2Q0wzdCtKK08rQ1FYY3RJQWhzWHdnY0dRZ3pySUlqNFZiMTN2MVc2V0pvWHl0MGNtelVEc2QyVTBLSllub2h5OVllZllSVFc0d2tZNEJhUXBrRFE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzQ5MDcwODEwOTcxOjc5QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlRxRU8vUVU4TElucURwZ2lhdnlIMTE0VXhEd1VuaktBbExQNGxUaDBadHcifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjE5NDQzNjcsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBRm1IIn0=" //Make sure session id starts with Byte;;;
 
 
 
@@ -81,8 +81,8 @@ module.exports = {
   caption: process.env.CAPTION || "*Powered By TalkDrove*",
   author: process.env.PACK_AUTHER || "TalkDrove",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "BYTE-MD",
-  ownername: process.env.OWNER_NAME || "TalkDrove",
+  botname: process.env.BOT_NAME || "么 Mr anonymous",
+  ownername: process.env.OWNER_NAME || "Mr anonymous",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
